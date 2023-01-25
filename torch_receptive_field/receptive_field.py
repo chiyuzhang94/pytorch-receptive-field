49ad2633a643abfa6ca451bc5d563d0d423204f9
Cihyu Zhang <zcy94@outlook.com>
2023-01-25 18:33:54 +0400

    debug

--- torch_receptive_field/receptive_field.py
+++ torch_receptive_field/receptive_field.py
@@ -127,8 +127,6 @@ def receptive_field(model, input_size, batch_size=-1, device="cuda"):
 
     # register hook
     model.apply(register_hook)
-
-    print(model)
     # make a forward pass
     model(x)
 
